466fa3269356b6197c103c719aad510abc77e315
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-07-05 11:11:45 +0300

    Uploaded to instance.

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,20 @@
-import supervisely as sly
+import glob
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import (
+    file_exists,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+)
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -53,21 +61,59 @@ def download_dataset(teamfiles_dir: str) -> str:
     return dataset_path
 
 
-
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/Users/iwatkot/Downloads/ninja/datasets/IntraretinalCystoidFluid/2021-training-data-ZA/2021-training-data-ZA"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    ds_name = "ds"
+    batch_size = 30
 
-    # ... some code here ...
+    images_folder_name = "images"
+    masks_folder_name = "masks"
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def create_ann(image_path):
+        labels = []
 
-    # return project
+        mask_folder = image_path.split(images_folder_name)[0] + masks_folder_name
+        mask_path = os.path.join(mask_folder, os.listdir(mask_folder)[0])
 
+        if file_exists(mask_path):
+            mask_np = sly.imaging.image.read(mask_path)[:, :, 0]
+            img_height = mask_np.shape[0]
+            img_wight = mask_np.shape[1]
+            mask = mask_np == 255
+            ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+            for i in range(1, ret):
+                obj_mask = curr_mask == i
+                curr_bitmap = sly.Bitmap(obj_mask)
+                curr_label = sly.Label(curr_bitmap, obj_class)
+                labels.append(curr_label)
 
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    obj_class = sly.ObjClass("cystic macular edema", sly.Bitmap)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_pathes = glob.glob(dataset_path + "/*/*/*.jpeg")
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+    for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+        img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
+
+        anns_batch = [create_ann(image_path) for image_path in img_pathes_batch]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ RELEASE_YEAR: int = 2022
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/zeeshanahmed13/intraretinal-cystoid-fluid"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1527991
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/intraretinal-cystoid-fluid"
@@ -29,7 +29,9 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/intraretinal-cystoid-fluid"
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/zeeshanahmed13/intraretinal-cystoid-fluid/download?datasetVersionNumber=3"
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://www.kaggle.com/datasets/zeeshanahmed13/intraretinal-cystoid-fluid/download?datasetVersionNumber=3"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
